4de83270ab2fd7bd28059dabe9bb2ca94106ff3d
Qiming Teng <tengqm@outlook.com>
2026-04-18 16:44:31 +0800

    Revise mt test again (#2513)

--- tools/setup_mthreads.sh
+++ tools/setup_mthreads.sh
@@ -3,6 +3,7 @@
 # TODO: Drop the following lines
 uv pip install -v --index $FLAGOS_PYPI \
   "torch==2.7.1+musa4.0.0" \
+  "torch_musa==2.7.1" \
   "flagtree==0.5.0+mthreads3.1"
 
 uv pip install -e .[mthreads,test]
